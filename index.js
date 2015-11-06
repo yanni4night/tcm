bd700008fe58224ed134f281e23afd10abddbe8f
yinyong02 <yinyong02@baidu.com>
2015-11-06 18:52:03 +0800

    support exist

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * changelog
  * 2015-10-21[00:22:06]:revised
  * 2015-11-06[16:40:24]:whole component name;yaml format instead of json
+ * 2015-11-06[16:59:23]:override confirm
  *
  * @author yanni4night@gmail.com
  * @version 0.1.0
@@ -15,32 +16,55 @@ var PZ = require('promzard').PromZard
 var path = require('path');
 var async = require('async');
 var jsYaml = require('js-yaml');
+var fs = require('fs');
+var targetFile = require('./target');
 
 var params = 'name,type,namespace,author'.split(',');
 
-async.parallel(params.map(function (key) {
-    return require('./lib/' + key + '.js');
-}), function (err, results) {
-    if (err) {
-        return process.exit(0);
+
+function prepare() {
+
+    if (fs.existsSync(targetFile)) {
+        read({
+            prompt: targetFile + ' already exists,override?',
+            default: 'no'
+        }, function (err, ret) {
+            if (ret && ret.toLowerCase().charAt(0) === 'y') {
+                work();
+            } else {
+                console.log('Aborted');
+            }
+        });
+    } else {
+        work();
     }
-    var ctx = {};
+};
 
-    results.forEach(function (v, idx) {
-        ctx[params[idx]] = v;
-    })
+function work() {
+    async.parallel(params.map(function (key) {
+        return require('./lib/' + key + '.js');
+    }), function (err, results) {
+        if (err) {
+            console.error(err);
+            return process.exit(0);
+        }
+        var ctx = {};
 
-    ask(ctx);
-});
+        results.forEach(function (v, idx) {
+            ctx[params[idx]] = v;
+        })
+
+        ask(ctx);
+    });
+
+}
 
 function ask(ctx) {
-    var pz = new PZ(path.join(__dirname, 'input.js'), ctx, function () {
-        console.log(arguments);
-    });
+    var pz = new PZ(path.join(__dirname, 'input.js'), ctx);
 
     pz.on('data', function (data) {
         read({
-            prompt: jsYaml.safeDump(data) + '\n\nIs this ok?',
+            prompt: '\n\n' + jsYaml.safeDump(data) + '\n\nIs this ok?',
             default: 'yes'
         }, function (err, ret) {
             if (!ret || ret.toLowerCase().charAt(0) === 'y') {
@@ -58,5 +82,7 @@ function ask(ctx) {
 }
 
 function write(config) {
-    require('fs').writeFileSync('module.yml', jsYaml.safeDump(config));
+    require('fs').writeFileSync(targetFile, jsYaml.safeDump(config));
 }
+
+prepare();
---
+++ target.js
@@ -0,0 +1,12 @@
+/**
+  * Copyright (C) 2015 tieba.baidu.com
+  * target.js
+  *
+  * changelog
+  * 2015-11-06[16:45:43]:revised
+  *
+  * @author yinyong02@baidu.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+module.exports = 'module.yml';
